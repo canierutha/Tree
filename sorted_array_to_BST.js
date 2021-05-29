fa1696028f4001356ae14b3d1281d407acd2db2a
canierutha <anierutha.c@gmail.com>
2021-05-30 01:03:36 +0530

    Update sorted_array_to_BST.js

--- sorted_array_to_BST.js
+++ sorted_array_to_BST.js
@@ -1,3 +1,4 @@
+//Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height
 class TreeNode {
  constructor(val) {
    this.val = val;
