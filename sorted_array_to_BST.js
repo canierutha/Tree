a91052d39feb2c7d2fc7020724e6d2e0a843c825
canierutha <anierutha.c@gmail.com>
2021-05-30 01:02:41 +0530

    Create sorted_array_to_BST.js

---
+++ sorted_array_to_BST.js
@@ -0,0 +1,25 @@
+class TreeNode {
+ constructor(val) {
+   this.val = val;
+   this.left = null;
+   this.right = null;
+   this.next = null;
+ }
+ 
+function create_minimal_BST(array) {
+ return create_BST_recursive(array, 0, array.length - 1);
+}
+ 
+function create_BST_recursive(array, start, end) {
+ let mid = 0;
+ mid = Math.ceil((start + end)/2);
+ let treeNode = new TreeNode(array[mid]);
+ // console.log(start, mid, end, treeNode);
+ if(start <= (mid - 1)) {
+   treeNode.left = create_BST_recursive(array, start, mid - 1);
+ }
+ if((mid + 1) <= end) {
+   treeNode.right = create_BST_recursive(array, mid + 1, end);
+ }
+ return treeNode;
+}
